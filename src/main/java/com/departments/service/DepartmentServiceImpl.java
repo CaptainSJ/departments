e393e10414991f0adec40946c0231d874fa96154
Saurabh Joshi <i.saurabhjoshi@hotmail.com>
2022-07-11 00:25:15 +0530

    Added Delete and Update end points

--- src/main/java/com/departments/service/DepartmentServiceImpl.java
+++ src/main/java/com/departments/service/DepartmentServiceImpl.java
@@ -1,32 +1,59 @@
 package com.departments.service;
 
-import com.departments.entity.Department;
-import com.departments.repo.DepartmentsRepo;
+import java.util.List;
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.departments.entity.Department;
+import com.departments.repo.DepartmentsRepo;
 
 @Service
-public class DepartmentServiceImpl implements DepartmentService{
+public class DepartmentServiceImpl implements DepartmentService {
 
-    @Autowired
-    DepartmentsRepo deptRepo;
+	@Autowired
+	DepartmentsRepo deptRepo;
 
+	@Override
+	public Department saveDept(Department dept) {
+		return deptRepo.save(dept);
+	}
 
-    @Override
-    public Department saveDept(Department dept) {
-        return deptRepo.save(dept);
-    }
+	@Override
+	public List<Department> getDept() {
+		return deptRepo.findAll();
+	}
 
-    @Override
-    public List<Department> getDept() {
-        return deptRepo.findAll();
-    }
+	@Override
+	public Department getDept(Long id) {
+		return deptRepo.findById(id).get();
 
-    @Override
-    public Department getDept(Long id) {
-        return deptRepo.findById(id).get();
-         
-    }
+	}
+
+	@Override
+	public void delDept(Long id) {
+		deptRepo.deleteById(id);
+
+	}
+
+	@Override
+	public Department updateDept(Long id, Department dept) {
+
+		Department deptDB = deptRepo.findById(id).get();
+
+		if (Objects.nonNull(dept.getDeptName()) && dept.getDeptName() != "") {
+			deptDB.setDeptName(dept.getDeptName());
+		}
+
+		if (Objects.nonNull(dept.getDeptCode()) && dept.getDeptCode() != "") {
+			deptDB.setDeptCode(dept.getDeptCode());
+		}
+
+		if (Objects.nonNull(dept.getDeptLocation()) && dept.getDeptLocation() != "") {
+			deptDB.setDeptLocation(dept.getDeptLocation());
+		}
+
+		return deptRepo.save(deptDB);
+	}
 }
